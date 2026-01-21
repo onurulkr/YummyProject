2018fdbc03f7fd207f10f4313b42829b062b33e1
onurulker-endizoom <55154950+onurulkr@users.noreply.github.com>
2026-01-22 01:35:42 +0300

    chef işlemleri ve yeni tema

---
+++ YummyProject.WebUI/ViewComponents/_ChefDefaultComponentPartial.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using YummyProject.WebUI.Dtos.ChefDtos;
+
+namespace YummyProject.WebUI.ViewComponents
+{
+    public class _ChefDefaultComponentPartial : ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _ChefDefaultComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7053/api/Chefs/");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonDta = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultChefDto>>(jsonDta);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
---
+++ YummyProject.WebUI/ViewComponents/_FooterDefaultComponentPartial.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using YummyProject.WebUI.Dtos.ServiceDtos;
+
+namespace YummyProject.WebUI.ViewComponents
+{
+    public class _FooterDefaultComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
+
